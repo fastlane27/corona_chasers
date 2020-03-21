29b03a1403d275dc1acbcdec1335ae75b73a3bf8
Brianna Fisher <Bnfisher4@gmail.com>
2020-03-21 17:32:55 -0500

    added empty views function

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+
+from .models import Comment, Country
+
+
+class CommentCreate(CreateView):
+    model = Comment
+    fields = '__all__'
+
+class CommentUpdate(UpdateView):
+  model = Comment
+  fields = ['posted_at', 'content']
+
+class CommentDelete(DeleteView):
+  model = Comment
+  success_url = '/countries/<int:country_id>/'
+
 
 def home(request):
     return render(request, 'home.html')
@@ -18,3 +36,22 @@ def signup(request):
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+def countries_index(request):
+  pass
+
+def countries_detail(request, country_id):
+  pass
+
+def profile_index(request):
+  pass
+
+def assoc_country(request, profile, countryy_id):
+  pass
+
+def unassoc_country(request, profile, country_id):
+  pass
+
+class CountryList(ListView):
+  model = Country
+
