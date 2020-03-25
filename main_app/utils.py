b7e19a09e1c205a408a831d6e48803b73f5c959f
ADAM <zebolsky.adam@gmail.com>
2020-03-25 13:15:36 -0500

    update utils

--- main_app/utils.py
+++ main_app/utils.py
@@ -3,17 +3,18 @@ import boto3
 
 S3_BASE_URL = 'https://s3-us-east-2.amazonaws.com/'
 BUCKET = 'coronachaser'
-SESSION = boto3.Session(profile_name='coronachaser').client('s3')
 
 
 def upload_file(file):
+    s3 = boto3.client('s3')
     key = uuid.uuid4().hex[:6] + file.name[file.name.rfind('.'):]
-    SESSION.upload_fileobj(file, BUCKET, key)
+    s3.upload_fileobj(file, BUCKET, key)
     url = f'{S3_BASE_URL}{BUCKET}/{key}'
     return url
 
 
 def delete_file(url):
+    s3 = boto3.client('s3')
     key = url.rsplit('/', 1)[-1]
-    SESSION.delete_object(Bucket=BUCKET, Key=key)
+    s3.delete_object(Bucket=BUCKET, Key=key)
     return
